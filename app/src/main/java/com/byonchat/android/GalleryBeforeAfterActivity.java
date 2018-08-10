956d842983a560b4f0ec16fe8416519bd81b90e4
Lukman Prayogo <lukman@dartmedia.biz>
2018-08-10 10:01:15 +0700

    compress file

--- app/src/main/java/com/byonchat/android/GalleryBeforeAfterActivity.java
+++ app/src/main/java/com/byonchat/android/GalleryBeforeAfterActivity.java
@@ -82,12 +82,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -401,19 +403,20 @@ public class GalleryBeforeAfterActivity extends Constants implements EmojiconGri
                     entity.addPart("parent_id", new StringBody(parent_id));
                 if (photos != null) {
                     for (NotesPhoto photo : photos) {
-                        if (!photo.getPhotoFile().exists()) {
+                        File file = new File(resizeAndCompressImageBeforeSend(getApplicationContext(), photo.getPhotoFile().getPath(), "fileUploadBC_" + new Date().getTime() + ".jpg"));
+                        if (!file.exists()) {
                             return null;
                         }
 
-                        int size = (int) photo.getPhotoFile().length();
+                        int size = (int) file.length();
 
                         byte[] bytes = new byte[size];
                         try {
-                            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(photo.getPhotoFile()));
+                            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
                             buf.read(bytes, 0, bytes.length);
                             buf.close();
 
-                            entity.addPart("photo_after", new FileBody(photo.getPhotoFile(), contentType, photo.getPhotoFile().getName()));
+                            entity.addPart("photo_after", new FileBody(file, contentType, photo.getPhotoFile().getName()));
                         } catch (FileNotFoundException e) {
                             // TODO Auto-generated catch block
                             e.printStackTrace();
@@ -578,5 +581,61 @@ public class GalleryBeforeAfterActivity extends Constants implements EmojiconGri
             return false;
         }
 
+    }
+
+    public static String resizeAndCompressImageBeforeSend(Context context, String filePath, String fileName) {
+        final int MAX_IMAGE_SIZE = 100 * 1024; // max final file size in kilobytes
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+        options.inSampleSize = calculateInSampleSize(options, 400, 400);
+        options.inJustDecodeBounds = false;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        Bitmap bmpPic = BitmapFactory.decodeFile(filePath, options);
+
+
+        int compressQuality = 100; // quality decreasing by 5 every loop.
+        int streamLength;
+        do {
+            ByteArrayOutputStream bmpStream = new ByteArrayOutputStream();
+            bmpPic.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream);
+            byte[] bmpPicByteArray = bmpStream.toByteArray();
+            streamLength = bmpPicByteArray.length;
+            compressQuality -= 5;
+        } while (streamLength >= MAX_IMAGE_SIZE);
+
+        try {
+            //save the resized and compressed file to disk cache
+            FileOutputStream bmpFile = new FileOutputStream(context.getCacheDir() + fileName);
+            bmpPic.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpFile);
+            bmpFile.flush();
+            bmpFile.close();
+        } catch (Exception e) {
+        }
+        //return the path of resized and compressed file
+        return context.getCacheDir() + fileName;
+    }
+
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        String debugTag = "MemoryInformation";
+        // Image nin islenmeden onceki genislik ve yuksekligi
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 }
--- app/src/main/java/com/byonchat/android/NoteCommentActivityV2.java
+++ app/src/main/java/com/byonchat/android/NoteCommentActivityV2.java
@@ -108,6 +108,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -115,6 +116,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -2458,15 +2460,16 @@ public class NoteCommentActivityV2 extends Constants implements EmojiconGridFrag
                     DataPart[] list = new DataPart[photos.size()];
                     int k = 0;
                     for (NotesPhoto photo : photos) {
-                        if (!photo.getPhotoFile().exists()) {
+                        File fileupload = new File(resizeAndCompressImageBeforeSend(getApplicationContext(), photo.getPhotoFile().getPath(), "fileUploadBC_" + new Date().getTime() + ".jpg"));
+                        if (!fileupload.exists()) {
                             return null;
                         }
 
-                        int size = (int) photo.getPhotoFile().length();
+                        int size = (int) fileupload.length();
 
                         byte[] bytes = new byte[size];
                         try {
-                            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(photo.getPhotoFile()));
+                            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(fileupload));
                             buf.read(bytes, 0, bytes.length);
                             buf.close();
 
@@ -2819,5 +2822,61 @@ public class NoteCommentActivityV2 extends Constants implements EmojiconGridFrag
         }
         ambilGambar ru = new ambilGambar();
         ru.execute(userid, id_note, id_comment, bc_user, idRoomTab, total_item);
+    }
+
+    public static String resizeAndCompressImageBeforeSend(Context context, String filePath, String fileName) {
+        final int MAX_IMAGE_SIZE = 100 * 1024; // max final file size in kilobytes
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+        options.inSampleSize = calculateInSampleSize(options, 400, 400);
+        options.inJustDecodeBounds = false;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+        Bitmap bmpPic = BitmapFactory.decodeFile(filePath, options);
+
+
+        int compressQuality = 100; // quality decreasing by 5 every loop.
+        int streamLength;
+        do {
+            ByteArrayOutputStream bmpStream = new ByteArrayOutputStream();
+            bmpPic.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream);
+            byte[] bmpPicByteArray = bmpStream.toByteArray();
+            streamLength = bmpPicByteArray.length;
+            compressQuality -= 5;
+        } while (streamLength >= MAX_IMAGE_SIZE);
+
+        try {
+            //save the resized and compressed file to disk cache
+            FileOutputStream bmpFile = new FileOutputStream(context.getCacheDir() + fileName);
+            bmpPic.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpFile);
+            bmpFile.flush();
+            bmpFile.close();
+        } catch (Exception e) {
+        }
+        //return the path of resized and compressed file
+        return context.getCacheDir() + fileName;
+    }
+
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        String debugTag = "MemoryInformation";
+        // Image nin islenmeden onceki genislik ve yuksekligi
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 }

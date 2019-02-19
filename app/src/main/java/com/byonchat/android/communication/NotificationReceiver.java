ca50d4eaaf579fd1d7f25bcf4454184d1b32a728
Iman Firmansyah <iman@hub9.biz>
2019-02-20 00:52:28 +0700

    hapus log

--- app/src/main/java/com/byonchat/android/communication/NotificationReceiver.java
+++ app/src/main/java/com/byonchat/android/communication/NotificationReceiver.java
@@ -124,13 +124,9 @@ public class NotificationReceiver extends BroadcastReceiver {
                     String h = cursorIS.getString(cursorIS.getColumnIndexOrThrow(BotListDB.ROOM_ICON));
 
                     try {
-                        Log.w("lala2", d);
                         JSONArray jsonArray = new JSONArray(d);
                         for (int iss = 0; iss < jsonArray.length(); iss++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(iss);
-                            Log.w("kasuk :: ", jsonObject.toString());
-                            //   Log.w("kasuk :: " + iss, jsonArray.getJSONObject(iss).getString("urutan"));
-
                             if (jsonObject.getString("id_rooms_tab").equalsIgnoreCase(asal[2])) {
 
                                 String aaa = b;
@@ -227,7 +223,6 @@ public class NotificationReceiver extends BroadcastReceiver {
 
                     } catch (JSONException e1) {
                         e1.printStackTrace();
-                        Log.w("lala33", e1.getMessage());
                     }
 
                 }

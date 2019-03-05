1357706db519b8a3850034ea8868c1d1d06e5c1e
Aziz <aziz@dartmedia.biz>
2019-03-05 09:41:52 +0700

    perbaikan desc. kartu nama

--- app/src/main/java/com/byonchat/android/personalRoom/FragmentMyCardID.java
+++ app/src/main/java/com/byonchat/android/personalRoom/FragmentMyCardID.java
@@ -203,11 +203,11 @@ public class FragmentMyCardID extends Fragment {
                                         textWarn.setText("Honda Pondok Indah tidak bertanggung jawab apabila customer melakukan pembayaran apapun melalui sales baik secara tunai maupun transfer ke rekening pribadi sales.");
                                     } else if (jsonOfficer.getString("lokasi").equalsIgnoreCase("HONDA FATMAWATI")) {
                                         Picasso.with(mContext).load("https://i0.wp.com/www.honda-ikb.com/baru/wp-content/uploads/elementor/thumbs/Page-BgTexture-nqj4cccw6nbm654ntwowspt9pau9kujusoc9pb241s.jpg?zoom=2&w=1170").into(imageLogo2);
-                                        textAddress.setText(Html.fromHtml("Jl. RS. Fatmawati No. 21 Jakarta Selatan, 12410\n" + "Telp. Showroom: (021) 7656456\n" + "Bengkel : (021) 7656437 \n" + "Fax : (021) 7502678"));
+                                        textAddress.setText(Html.fromHtml("Jl. RS. Fatmawati No. 21 Jakarta Selatan, 12410\n" + "Telp. Showroom : (021) 7656456,\n" + "Bengkel : (021) 7656437,\n" + "Fax : (021) 7502678"));
                                         textWarn.setText("Honda Fatmawati tidak bertanggung jawab apabila customer melakukan pembayaran apapun melalui sales baik secara tunai maupun transfer ke rekening pribadi sales.");
                                     } else if (jsonOfficer.getString("lokasi").equalsIgnoreCase("HONDA PRADANA SAWANGAN")) {
                                         imageLogo2.setVisibility(View.GONE);
-                                        textAddress.setText(Html.fromHtml("Jl.Raya Cinangka No.9 Serua Bojong Sari\n" + "Depok-Jawa Barat 16517\n" + "Telp. (021) 3049 8889, (021) 3049 9990\n" + "Fax : (021) 3042 8889"));
+                                        textAddress.setText(Html.fromHtml("Jl.Raya Cinangka No.9 Serua Bojong Sari\n" + "Depok-Jawa Barat 16517\n" + "Telp. (021) 3049 8889, (021) 3049 9990,\n" + "(021) 3042 8889"));
                                         textWarn.setText("Honda Pradana Sawangan tidak bertanggung jawab apabila customer melakukan pembayaran apapun melalui sales baik secara tunai maupun transfer ke rekening pribadi sales.");
                                     }
 

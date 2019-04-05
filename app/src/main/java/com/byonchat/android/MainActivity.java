9c207fecb02739a42b7d9a7fb3a2be4723a8f670
Iman Firmansyah <iman@hub9.biz>
2019-04-05 15:24:45 +0700

    ini juga bikin error logo

--- app/src/main/java/com/byonchat/android/MainActivity.java
+++ app/src/main/java/com/byonchat/android/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 if (cursorSkin.getCount() == 0 || insertByon) {
                     Bitmap logos = BitmapFactory.decodeResource(getResources(), R.drawable.logo_byon);
                     Bitmap back = BitmapFactory.decodeResource(getResources(), R.drawable.bg_chat_baru);
-                    Bitmap header = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+                    Bitmap header = BitmapFactory.decodeResource(getResources(), R.drawable.logo_byon);
                     Skin skin = new Skin("byonchat", "original", "#006b9c", logos, header, back);
                     db.createSkin(skin);
                 }

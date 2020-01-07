9f2dd6466dcddc9e37513dc4608c9b89ccc79bdb
Iman Firmansyah <iman@hub9.biz>
2020-01-07 16:26:10 +0700

    new type  copy_field_dropdown

--- app/src/main/java/com/byonchat/android/FragmentDinamicRoom/DinamicRoomTaskActivity.java
+++ app/src/main/java/com/byonchat/android/FragmentDinamicRoom/DinamicRoomTaskActivity.java
@@ -108,6 +108,7 @@ import com.byonchat.android.communication.NotificationReceiver;
 import com.byonchat.android.createMeme.FilteringImage;
 import com.byonchat.android.helpers.Constants;
 import com.byonchat.android.list.AttachmentAdapter;
+import com.byonchat.android.list.ConversationAdapter;
 import com.byonchat.android.list.IconItem;
 import com.byonchat.android.list.utilLoadImage.ImageLoaderLarge;
 import com.byonchat.android.location.ActivityDirection;
@@ -6574,6 +6575,192 @@ public class DinamicRoomTaskActivity extends AppCompatActivity implements Locati
                         valSetOne.add(String.valueOf(linearLayout.getChildCount()));
                         linearLayout.addView(view, params2);
 
+                        hashMap.put(Integer.parseInt(idListTask), valSetOne);
+
+                    }else if (type.equalsIgnoreCase("copy_field_dropdown")) {
+                        // TODO: 2019-10-21 Membuat default value dan mengembalikan copy_field
+
+                        TextView textView = new TextView(DinamicRoomTaskActivity.this);
+                        if (required.equalsIgnoreCase("1")) {
+                            label += "<font size=\"3\" color=\"red\">*</font>";
+                        }
+                        textView.setText(Html.fromHtml(label));
+                        textView.setTextSize(15);
+
+                        if (count == null) {
+                            count = 0;
+                        } else {
+                            count++;
+                        }
+
+                        et[count] = (EditText) getLayoutInflater().inflate(R.layout.edit_input_layout, null);
+
+                        List<String> valSetOne = new ArrayList<String>();
+                        valSetOne.add(String.valueOf(count));
+                        valSetOne.add(required);
+                        valSetOne.add(type);
+                        valSetOne.add(name);
+                        valSetOne.add(label);
+                        valSetOne.add(String.valueOf(i));
+
+                        et[count].setId(Integer.parseInt(idListTask));
+                        et[count].setHint(placeHolder);
+
+
+                        if (flag.equalsIgnoreCase("new_dropdown_dinamis")) {
+                            et[count] = (EditText) getLayoutInflater().inflate(R.layout.edit_input_layout, null);
+                            et[count].setMinLines(4);
+                            et[count].setMaxLines(8);
+                            et[count].setScroller(new Scroller(context));
+                            et[count].setVerticalScrollBarEnabled(true);
+                            et[count].setSingleLine(false);
+                            final int finalI7 = i;
+                            et[count].setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    String defaultValue = "{}";
+                                    Cursor cursorCildValue = db.getSingleRoomDetailFormWithFlagContent(idDetail, username, idTab, "cild", jsonCreateType(idListTask, type, String.valueOf(finalI7)));
+                                    if (cursorCildValue.getCount() > 0) {
+                                        if (cursorCildValue.getString(cursorCildValue.getColumnIndexOrThrow(BotListDB.ROOM_DETAIL_CONTENT)).length() > 0) {
+                                            defaultValue = cursorCildValue.getString(cursorCildValue.getColumnIndexOrThrow(BotListDB.ROOM_DETAIL_CONTENT));
+                                        }
+                                    }
+
+                                    FragmentManager fm = getSupportFragmentManager();
+                                    /*DialogNewDropdown testDialog = new DialogNewDropdown(new DialogNewDropdown.finishListener() {
+                                        @Override
+                                        public void submitted(String json) {
+                                            if (ConversationAdapter.isJSONValid(json)) {
+                                                JSONObject jsonObject = null;
+                                                try {
+                                                    jsonObject = new JSONObject(json);
+
+                                                    Iterator<String> keys = jsonObject.keys();
+
+                                                    List<String> keysList = new ArrayList<String>();
+                                                    while (keys.hasNext()) {
+                                                        keysList.add(keys.next());
+                                                    }
+
+                                                    String resultNya = "";
+                                                    for (String aa : keysList) {
+
+                                                        resultNya += String.valueOf(aa) + " = " + jsonObject.getString(aa) + "\n";
+
+                                                    }
+                                                    et[Integer.valueOf(valSetOne.get(0))].setText(resultNya);
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            }
+
+                                            Cursor cEdit = db.getSingleRoomDetailFormWithFlagContent(idDetail, username, idTab, "cild", jsonCreateType(idListTask, type, String.valueOf(finalI7)));
+                                            if (cEdit.getCount() > 0) {
+                                                RoomsDetail orderModel = new RoomsDetail(idDetail, idTab, username, json, jsonCreateType(idListTask, type, String.valueOf(finalI7)), name, "cild");
+                                                db.updateDetailRoomWithFlagContent(orderModel);
+                                            } else {
+                                                RoomsDetail orderModel = new RoomsDetail(idDetail, idTab, username, json, jsonCreateType(idListTask, type, String.valueOf(finalI7)), name, "cild");
+                                                db.insertRoomsDetail(orderModel);
+                                            }
+
+                                        }
+                                    }, 5 + "", defaultValue);
+                                    testDialog.setRetainInstance(true);
+                                    testDialog.show(fm, "Dialog");*/
+                                }
+                            });
+
+
+                        } else {
+                            et[count].setFilters(new InputFilter[]{new InputFilter.LengthFilter(Integer.parseInt(maxlength))});
+                        }
+
+
+                        Cursor cursorCild = db.getSingleRoomDetailFormWithFlagContent(idDetail, username, idTab, "cild", jsonCreateType(idListTask, type, String.valueOf(i)));
+                        Boolean copyDari = false;
+
+
+                        if (cursorCild.getCount() > 0) {
+                            if (cursorCild.getString(cursorCild.getColumnIndexOrThrow(BotListDB.ROOM_DETAIL_CONTENT)).length() > 0) {
+                                et[count].setText(cursorCild.getString(cursorCild.getColumnIndexOrThrow(BotListDB.ROOM_DETAIL_CONTENT)));
+                            }
+                        } else {
+                            copyDari = true;
+                        }
+
+                        if (copyDari) {
+                            Cursor cursorValue = db.getSingleRoomDetailFormWithFlag(idDetail, username, idTab, "value");
+                            if (cursorValue.getCount() > 0) {
+                                String valUEParent = "";
+                                final String contentValue = cursorValue.getString(cursorValue.getColumnIndexOrThrow(BotListDB.ROOM_CONTENT));
+                                JSONArray jsonArrayYes = null;
+                                try {
+                                    jsonArrayYes = new JSONArray(contentValue);
+                                    for (int ii = (jsonArrayYes.length() - 1); ii >= 0; ii--) {
+
+                                        JSONArray magic = new JSONArray(jsonArrayYes.getJSONArray(ii).toString());
+                                        JSONObject oContent2 = new JSONObject(magic.get(1).toString());
+                                        JSONArray joContent = oContent2.getJSONArray("value_detail");
+
+                                        for (int iff = 0; iff < joContent.length(); iff++) {
+                                            final String idValue = joContent.getJSONObject(iff).getString("id").toString();
+                                            String pareen = jsonArray.getJSONObject(i).getString("copy_from").toString();
+                                            if (idValue.equalsIgnoreCase(pareen)) {
+                                                valUEParent = joContent.getJSONObject(iff).getString("value").toString();
+                                            }
+                                        }
+
+                                    }
+                                } catch (Exception c) {
+                                }
+
+
+                                et[count].setText(valUEParent);
+                                RoomsDetail orderModel = new RoomsDetail(idDetail, idTab, username, valUEParent, jsonCreateType(idListTask, type, String.valueOf(i)), name, "cild");
+                                db.insertRoomsDetail(orderModel);
+                            }
+                        }
+
+
+                        if ((!showButton)) {
+                            et[count].setEnabled(false);
+                        }
+
+                        String chanc = et[Integer.valueOf(valSetOne.get(0))].getText().toString();
+
+                        if (ConversationAdapter.isJSONValid(chanc)) {
+                            JSONObject jsonObject = new JSONObject(chanc);
+                            Iterator<String> keys = jsonObject.keys();
+
+                            List<String> keysList = new ArrayList<String>();
+                            while (keys.hasNext()) {
+                                keysList.add(keys.next());
+                            }
+
+                            String resultNya = "";
+                            for (String aa : keysList) {
+
+                                resultNya += String.valueOf(aa) + " = " + jsonObject.getString(aa) + "\n";
+
+                            }
+                            et[Integer.valueOf(valSetOne.get(0))].setText(resultNya);
+                        }
+
+
+                        String formulas = jsonArray.getJSONObject(i).getString("formula").toString();
+                        if (formulas.equalsIgnoreCase("disable")) {
+                            et[count].setEnabled(false);
+                        }
+                        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        params1.setMargins(30, 10, 30, 0);
+                        LinearLayout.LayoutParams params2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        params2.setMargins(30, 10, 30, 40);
+
+
+                        valSetOne.add(String.valueOf(linearLayout.getChildCount()));
+                        linearLayout.addView(textView, params1);
+                        valSetOne.add(String.valueOf(linearLayout.getChildCount()));
+                        linearLayout.addView(et[count], params2);
                         hashMap.put(Integer.parseInt(idListTask), valSetOne);
 
                     } else if (type.equalsIgnoreCase("new_dropdown_dinamis")) {

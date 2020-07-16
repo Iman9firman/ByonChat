7078d3ca5a6ec29f6260b84cb22bf844bbeeb19c
Iman Firmansyah <iman@hub9.biz>
2020-07-16 14:49:38 +0700

    perbaikan ssl

--- app/src/main/java/com/byonchat/android/utils/ClientSSLSocketFactory.java
+++ app/src/main/java/com/byonchat/android/utils/ClientSSLSocketFactory.java
@@ -2,18 +2,28 @@ package com.byonchat.android.utils;
 
 import android.net.SSLCertificateSocketFactory;
 import android.net.SSLSessionCache;
+import android.util.Log;
 
 import com.byonchat.android.application.Application;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 public class ClientSSLSocketFactory extends SSLCertificateSocketFactory {
@@ -29,27 +39,64 @@ public class ClientSSLSocketFactory extends SSLCertificateSocketFactory {
 
     public static SSLSocketFactory getSocketFactory() {
         try {
-            X509TrustManager tm = new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-                }
-
-                public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
-                }
-
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-            };
-            sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[]{tm}, null);
-
-            SSLSocketFactory ssf = ClientSSLSocketFactory.getDefault(10000, new SSLSessionCache(Application.getInstance()));
-
-            return ssf;
-        } catch (Exception ex) {
-            return null;
+            KeyStore keyStore = KeyStore.getInstance("BKS");
+            keyStore.load(null, null);
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+            TrustManager[] wrappedTrustManagers = getWrappedTrustManagers(tmf.getTrustManagers());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, wrappedTrustManagers, null);
+            return sslContext.getSocketFactory();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
     }
+
+    private static TrustManager[] getWrappedTrustManagers(TrustManager[] trustManagers) {
+        final X509TrustManager originalTrustManager = (X509TrustManager) trustManagers[0];
+        return new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return originalTrustManager.getAcceptedIssuers();
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            if (certs != null && certs.length > 0) {
+                                certs[0].checkValidity();
+                            } else {
+                                originalTrustManager.checkClientTrusted(certs, authType);
+                            }
+                        } catch (CertificateException e) {
+                            Log.w("checkClientTrusted", e.toString());
+                        }
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        try {
+                            if (certs != null && certs.length > 0) {
+                                certs[0].checkValidity();
+                            } else {
+                                originalTrustManager.checkServerTrusted(certs, authType);
+                            }
+                        } catch (CertificateException e) {
+                            Log.w("checkServerTrusted", e.toString());
+                        }
+                    }
+                }
+        };
+    }
+
 
     @Override
     public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
@@ -61,3 +108,5 @@ public class ClientSSLSocketFactory extends SSLCertificateSocketFactory {
         return sslContext.getSocketFactory().createSocket();
     }
 }
+
+
